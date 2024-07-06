0743686738c6f3ccb395c153840bf983d817d450
AndrewWarrington33 <andrewwarrington45@gmail.com>
2024-07-06 14:21:46 -0400

    Added Rscript for analysis

--- code/analysis.R
+++ code/analysis.R
@@ -1,63 +1,76 @@
-#Data Analysis Project Script
+# Data Analysis Project Script
 
-anole <- read.csv("cox_functional_ecology_data.csv")
+anole <- read.csv("data/cox_functional_ecology_data.csv")
 ovx <- subset(anole, Treat == "OVX")
 sham <- subset(anole, Treat == "SHAM")
 
-#Initial Data Exploration
+# Initial Data Exploration
 summary(ovx)
 summary(sham)
 
-#Growth (mm) five number summary for ovx:
-#min: -1.0
-#1st Quart: 2.0
-#Median: 2.0
-#3rd Quart: 3.0
-#Max: 7.0
-#mean is 2.309
-#sd is 1.274607
+# Growth (mm) five number summary for ovx:
+# min: -1.0
+# 1st Quart: 2.0
+# Median: 2.0
+# 3rd Quart: 3.0
+# Max: 7.0
+# mean is 2.309
+# sd is 1.274607
 
-sd(ovx$Growth..mm.,na.rm = TRUE) par(mfrow=c(1,2))
+# Plotting the OVX Growth data
+sd(ovx$Growth..mm., na.rm = TRUE)
+par(mfrow = c(1, 2))
 
-hist(ovx$Growth..mm., main="Change in Body Length of OVX Lizards", xlab="Growth (mm)", col="blue")
+hist(ovx$Growth..mm., main = "Change in Body Length of OVX Lizards", xlab = "Growth (mm)", col = "blue")
 
-boxplot(ovx$Growth..mm., main="Change in Body Length of OVX Lizards", ylab="Growth (mm)", col="green")
+boxplot(ovx$Growth..mm., main = "Change in Body Length of OVX Lizards", ylab = "Growth (mm)", col = "green")
 
-#The histogram and boxplot visualizations indicate a unimodal, right-skewed distribution. There are at least 3 high outliers and at least
 
-2 low outliers.
+# Plotting the SHAM growth data
+sd(sham$Growth..mm., na.rm = TRUE)
 
-#The center appears to be around 2.0 mm of growth, with a mean value of 2.309 mm, and a median value of 2.0 mm of growth. 
-#The mean being of higher value than the median is also indicative of a right-skewed distribution.
+hist(sham$Growth..mm., main = "Change in Body Length of SHAM Lizards", xlab = "Growth (mm)", col = "cyan")
 
-#Growth (mm) 5 number summar for sham:
+boxplot(sham$Growth..mm., main = "Change in Body Length of SHAM Lizards", ylab = "Growth (mm)", col = "green")
 
-#Min -1.0
 
-#1st Quart: 1.0
 
-#Median: 1.0
+# Calculating T test statistic:
 
-#3rd Quart: 2.0
+# Calling ovx group 1 and sham group 2
+# T_ts
+# ovx: n = 139, (140 observations - 1 NA)
+# sham: n = 121, (122 observations - 1 NA)
 
-#Max: 4.0
+(2.309 - 1.426) # 0.883
 
-#mean is 1.426
+(1.274607^2) / (139) + (1.042156^2) / (121) # 0.02066388
 
-#sd is 1.042156
+sqrt(0.02066388) # 0.1688002
 
-sd(sham$Growth..mm.,na.rm=TRUE)
+0.883 / 0.1437494 # 6.142634
 
-hist(sham$Growth..mm., main="Change in Body Length of SHAM Lizards", xlab="Growth (mm)", col="cyan")
+# T_ts = 6.142634
+# Df = 120
 
-boxplot(sham$Growth..mm., main="Change in Body Length of SHAM Lizards", ylab="Growth (mm)", col="green")
 
-#The histogram and boxplot visualizations indicate the growth for the sham group follows a bimodal distribution. 
-#There is at least one high outlier and one low outlier. There are two centers, one around 1.0 mm and a center closer to 2.0 mm. 
-#The growth for sham group has a mean growth of 1.426 mm and a median growth of 1.0 mm. #Hypothesis Test and Confidence Interval
 
-# H_0: mu_ovx = mu_sham
+# P-value Calculation
+pt(6.142634, df = 120, lower.tail = FALSE)
 
-# H_a: mu_ovx > mu_sham
+# one-sided, right-tailed t test
+# P-value = 5.445521e-09
+# significance level of 5% or 0.05 (alpha)
+# p-value < 0.05
 
-# Is there sufficient evidence to conclude that the mean growth of the ovx group is significantly higher than the mean sham growth?
+
+# Confidence interval calculation: 95% CI
+
+# Calculate t*
+qt(.025, df = 120)
+
+# t* = -1.97993
+
+-1.97993 * 0.1437494 # equals m (margin of error) -> -0.2846137
+(2.309 - 1.426) + (-0.2846137) # 0.5983863, lower bound of 95% CI
+(2.309 - 1.426) - (-0.2846137) # 1.167614, upper bound of 95% CI
